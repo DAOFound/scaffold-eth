58bac7f58cb87a6e95d88f6e0b60552012e7eb49
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-04-10 15:46:10 +0200

    Unlock implemented

--- packages/react-app/src/components/ProposalDetails.jsx
+++ packages/react-app/src/components/ProposalDetails.jsx
@@ -1,8 +1,31 @@
+import { Button } from "antd";
+import { useEffect, useState } from "react";
 import Address from "./Address";
 import { ProposalExecuteButton } from "./ProposalExecuteButton";
 import { ProposalVoteButton } from "./ProposalVoteButton";
 
 export function ProposalDetails({ proposal, mainnetProvider, writeContracts, tx }) {
+  const [locked, setLocked] = useState("locked");
+
+  const checkout = () => {
+    window.unlockProtocol && window.unlockProtocol.loadCheckoutModal();
+  };
+
+  useEffect(() => {
+    const checkUnlock = async () => {
+      try {
+        await window.unlockProtocol;
+        if (window.unlockProtocol) {
+          setLocked(window.unlockProtocol.getState());
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    checkUnlock();
+  }, []);
+
   var completedContent;
   if (proposal.completed) {
     completedContent = (
@@ -14,8 +37,13 @@ export function ProposalDetails({ proposal, mainnetProvider, writeContracts, tx 
   } else {
     completedContent = (
       <>
-        <ProposalVoteButton proposal={proposal} writeContracts={writeContracts} tx={tx} /><br />
-        <ProposalExecuteButton proposal={proposal} writeContracts={writeContracts} tx={tx} />
+        <ProposalVoteButton proposal={proposal} writeContracts={writeContracts} tx={tx} />
+        <br />
+        {locked === "locked" ? (
+          <Button onClick={checkout}>Unlock Execution</Button>
+        ) : (
+          <ProposalExecuteButton proposal={proposal} writeContracts={writeContracts} tx={tx} />
+        )}
       </>
     );
   }
